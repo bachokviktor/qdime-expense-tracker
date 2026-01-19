7f7a29ed767d125c889b7529ef4315bd6068ee0d
Bachok Viktor <bachok.viktor@gmail.com>
2026-01-19 20:25:55 +0200

    Implement user authentication and registration

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,8 @@ from . import views
 app_name = "users"
 
 urlpatterns = [
-    path("profile/", views.ProfileView.as_view(), name="profile")
+    path("profile/", views.ProfileView.as_view(), name="profile"),
+    path("login/", views.LoginView.as_view(), name="login"),
+    path("registration/", views.RegistrationView.as_view(), name="registration"),
+    path("logout/", views.LogoutView.as_view(), name="logout")
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,64 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Create your views here.
-class ProfileView(View):
+class ProfileView(LoginRequiredMixin, View):
+    login_url = "users:login"
     template_name = "users/profile.html"
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
+
+
+class LoginView(View):
+    template_name = "users/login.html"
+
+    def get(self, request, *args, **kwargs):
+        form = AuthenticationForm()
+
+        return render(request, self.template_name, {"form": form})
+
+    def post(self, request, *args, **kwargs):
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+            login(request, form.get_user())
+
+            next_url = request.POST.get("next")
+
+            if next_url:
+                return redirect(next_url)
+            else:
+                return redirect("core:home")
+
+        return render(request, self.template_name, {"form": form})
+
+
+class RegistrationView(View):
+    template_name = "users/registration.html"
+
+    def get(self, request, *args, **kwargs):
+        form = UserCreationForm()
+
+        return render(request, self.template_name, {"form": form})
+
+    def post(self, request, *args, **kwargs):
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            login(request, form.save())
+
+            return redirect("core:home")
+
+        return render(request, self.template_name, {"form": form})
+
+
+class LogoutView(View):
+    def post(self, request, *args, **kwargs):
+        logout(request)
+
+        return redirect("core:home")
